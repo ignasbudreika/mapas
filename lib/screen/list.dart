65be1d103e2db30e9e8ec401db035167dfa77e64
LaurynasDenisenko <laurynas.denisenko@ktu.edu>
2021-04-19 14:31:24 +0300

    edited button element

--- lib/screen/list.dart
+++ lib/screen/list.dart
@@ -1,50 +1,53 @@
 import 'package:flutter/material.dart';
 
-class List extends StatelessWidget {
+
+class EventsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Event list';
-
     return MaterialApp(
-      title: title,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: ListView(
-          children: <Widget>[
-            ListTile(
-              leading: Icon(Icons.sports_basketball),
-              title: Text('Meet to play'),
+        body: Column(children: [
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.sports_basketball),
+                  title: Text('Meet to play'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.audiotrack),
+                  title: Text('Rockband concert'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.chat),
+                  title: Text('Team meet'),
+                ),
+              ],
             ),
-            ListTile(
-              leading: Icon(Icons.audiotrack),
-              title: Text('Rockband concert'),
-            ),
-            ListTile(
-              leading: Icon(Icons.chat),
-              title: Text('Chat with strangers'),
-            ),
-            Button(),
-          ],
-        ),
+          ),
+          Button(),
+        ]),
       ),
     );
   }
 }
 
 class Button extends StatelessWidget {
-
   @override
-  Widget build(BuildContext context){
-    return Align(alignment: Alignment.bottomRight,
-    child: RaisedButton(onPressed: null,
-    child: const Text('+', style: TextStyle(fontSize: 20)),
-    shape: CircleBorder(),
-    color: Colors.green,
-    textColor: Colors.white,
-    elevation: 3,
-    ),
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomRight,
+      child: MaterialButton(
+              onPressed: () {},
+              color: Colors.green,
+              textColor: Colors.white,
+              child: Icon(
+                Icons.add,
+                size: 45,
+              ),
+              padding: EdgeInsets.all(10),
+              shape: CircleBorder(),
+          ),
     );
   }
 }
